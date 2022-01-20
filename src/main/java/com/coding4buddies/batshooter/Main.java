c5823f7c204c2f6aa0002d1173bb3a05e97b057a
MinPen <grimaldo.131810@kalibo.sti.edu>
2022-01-21 01:31:08 +0800

    Initialize the Splash Screen

--- src/main/java/com/coding4buddies/batshooter/Main.java
+++ src/main/java/com/coding4buddies/batshooter/Main.java
@@ -1,14 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    Date: January 21, 2021 
+    Created by: MinPen
+    Purpose: The Main Class of the Game
+*/
+
 package com.coding4buddies.batshooter;
 
-/**
- *
- * @author jayden_jj22yahoo.com
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
---
+++ src/main/java/com/coding4buddies/batshooter/SplashScreen.java
@@ -0,0 +1,12 @@
+/*
+    Date: January 21, 2021 
+    Created by: MinPen
+    Purpose: Loading Screen of the Game
+*/
+
+package com.coding4buddies.batshooter;
+
+
+public class SplashScreen {
+    
+}
