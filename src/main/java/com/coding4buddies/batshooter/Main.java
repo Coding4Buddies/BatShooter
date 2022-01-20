908dc400f430a229f518a0fd7b074dc4486e4a17
MinPen <grimaldo.131810@kalibo.sti.edu>
2022-01-21 00:13:15 +0800

    Initialize the Project

---
+++ src/main/java/com/coding4buddies/batshooter/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coding4buddies.batshooter;
+
+/**
+ *
+ * @author jayden_jj22yahoo.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
