4a293aa0168823e16192a24363e10e64d3dab0b5
MinPen <93776264+nickichann01@users.noreply.github.com>
2022-01-23 00:11:07 +0800

    Created Splash Screen (#1)
    * Initialize the Splash Screen
    
    * Finish Up the Splash Screen
    
    * Change the Bg Image, Font Size and Color of Label
    
    * Change the Bg Image
    
    * Change the Bg Image
    
    * Revert "Change the Bg Image"
    
    This reverts commit 171af4a9ce021ed790f7d98b3201e32d1bdd2a78.
    
    * Update Bg Image
    
    * Update Bg Image
    
    * Finishing Up the Splash Screen
    
    Co-authored-by: MinPen <grimaldo.131810@kalibo.sti.edu>
    Co-authored-by: Jookie262 <barrientos.134038@iloilo.sti.edu.ph>

---
+++ src/main/java/com/coding4buddies/batshooter/SplashScreen.java
@@ -0,0 +1,110 @@
+/*
+    Date: January 21, 2021 
+    Created by: MinPen
+    Purpose: Loading Screen of the Game
+*/
+
+package com.coding4buddies.batshooter;
+
+import java.awt.*;
+import java.util.Random;
+import javax.swing.*;
+
+public class SplashScreen extends JFrame {
+    
+    // Initializing the variables needed in Splash Screen
+    JProgressBar progressBar = new JProgressBar(0, 200);
+    JLabel label = new JLabel("Did you know that Baby bats are called pups!");
+        
+    // Setting up the Splash Screen
+    public SplashScreen() {
+       this.setUndecorated(true);
+       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       this.setSize(800, 500);
+       this.setLocationRelativeTo(null);
+       setBackground();
+       addLoading();
+       changeText();
+    }
+    
+    // Method that Sets Image Background to the Splash Screen
+    public void setBackground(){
+        this.setContentPane(new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+               Image i = new ImageIcon("res/image_splash.jpg").getImage();			   
+               super.paintComponent(g);
+               g.drawImage(i,0,0,null);
+            }
+       });
+    }
+    
+    // Method that insert the progress bar and label
+    public void addLoading(){
+
+        // Customize JLabel
+        label.setFont(new Font("Arial", Font.BOLD, 15));
+        label.setForeground(Color.WHITE);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Customize JProgressBar
+        progressBar.setMaximumSize(new Dimension(350,10));
+        progressBar.setForeground(new Color(130, 192, 207));
+        progressBar.setBackground(new Color(12, 24,68));
+        progressBar.setBorderPainted(false);
+        progressBar.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        this.add(Box.createVerticalStrut(350));
+        this.add(label);
+        this.add(Box.createVerticalStrut(10));
+        this.add(progressBar);
+    }
+    
+    // Method that makes the progress bar load
+    public void iterate(){
+        int i = 0;
+        try {
+            while(i <= 210){
+                checkPercent(i);
+                progressBar.setValue(i);
+                Thread.sleep(60);
+                i++;
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // Method that changes the text every 2 seconds
+    public void changeText(){
+        String[] facts = {
+                "Did you know that bats can find their food in total darkness.",
+                "Did you know that Pallid bats eat scorpions!",
+                "Did you know that there are over 1,400 species of bats worldwide.",
+                "Did you know that not all bats hibernate."
+        };
+
+       Timer t = new Timer(2000, e -> {
+           Random rand = new Random();
+           String get = facts[rand.nextInt(facts.length)];
+           label.setText(get);
+       });
+
+       t.setRepeats(true);
+       t.start();
+    }
+    
+    // Method that checks the percent in the Progress Bar
+    public void checkPercent(int percent){
+        if(percent == 210) {
+            closeSplashScreen();
+        }
+    }
+    
+    // Method that dispose and close the Splash Screen
+    public void closeSplashScreen(){
+        this.dispose();
+        System.exit(0);
+    }
+}
