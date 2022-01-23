0914e29f27e886b45adaace7b996365c73db8427
nickichann01 <grimaldo.131810@kalibo.sti.edu>
2022-01-23 21:55:36 +0800

    Initializing the Level Panel for the Game

---
+++ src/main/java/mainscreenpanel/LevelPanel.java
@@ -0,0 +1,16 @@
+/*
+    Date: January 23, 2021 
+    Created by: MinPen
+    Purpose: Level Panel (Consist of 9 Levels) for the MainScreen Frame
+*/
+
+package mainscreenpanel;
+
+public class LevelPanel{
+   
+   public LevelPanel(){
+      
+   }
+   
+   
+}
